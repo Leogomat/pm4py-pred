e0e65061cd1d9d0f06bdb2efdb4368e5674bff8b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-02 17:02:53 +0200

    Added init files

---
+++ pm4pypred/__init__.py
@@ -0,0 +1,8 @@
+from pm4pypred import algo
+
+__version__ = '0.0.1'
+__doc__ = "Process Mining for Python - Prediction algorithms"
+__author__ = 'PADS'
+__author_email__ = 'pm4py@pads.rwth-aachen.de'
+__maintainer__ = 'PADS'
+__maintainer_email__ = "pm4py@pads.rwth-aachen.de"
